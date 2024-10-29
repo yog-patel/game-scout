45232f30610e4b91017386ac560413e207f7494d
yog-patel <122759187+yog-patel@users.noreply.github.com>
2024-10-29 15:42:19 +0530

    Update SupabaseClient2.js
    API management_1.0

--- src/DBConnect/SupabaseClient2.js
+++ src/DBConnect/SupabaseClient2.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase2 = createClient(
-    "https://szksothoimyzawwdympv.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN6a3NvdGhvaW15emF3d2R5bXB2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyODg4NTg3MSwiZXhwIjoyMDQ0NDYxODcxfQ.KiF7_97JtOKQh-DFQVThpkM8sf8CnhZ-qPcdoqWn88o"
+    SECRET_API_KEY_1,
+    SECRET_API_KEY_3
 );
