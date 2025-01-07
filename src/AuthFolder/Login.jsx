885b5bf5f940657482af34aaef22358bed0c8c9a
yog-patel <122759187+yog-patel@users.noreply.github.com>
2025-01-08 01:47:22 +0530

    Updated Login.jsx to remove github login feature

--- src/AuthFolder/Login.jsx
+++ src/AuthFolder/Login.jsx
@@ -60,7 +60,7 @@ function LoginPage() {
     const handleSignUp = async (e) => {
         e.preventDefault();
         try {
-            const { data, error } = await supabase2.auth.signUp({
+            const { error } = await supabase2.auth.signUp({
                 email,
                 password,
                 options: {
@@ -118,15 +118,15 @@ function LoginPage() {
             {/*<h2>{isSignUp ? "Create Account" : "Login Page"}</h2>*/}
 
             {/* GitHub Login Button fill="currentColor"*/}
-            <div className={"git-login-container"}>
-            <button className={"git-signup-submit-button"} onClick={handleGithubLogin}>Login with GitHub
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                     className="bi bi-github" viewBox="0 0 16 16">
-                    <path
-                        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
-                </svg>
-            </button>
-            </div>
+            {/*<div className={"git-login-container"}>*/}
+            {/*<button className={"git-signup-submit-button"} onClick={handleGithubLogin}>Login with GitHub*/}
+            {/*    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"*/}
+            {/*         className="bi bi-github" viewBox="0 0 16 16">*/}
+            {/*        <path*/}
+            {/*            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>*/}
+            {/*    </svg>*/}
+            {/*</button>*/}
+            {/*</div>*/}
             {/* Form for Login or Signup */}
             {/*style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem' }}*/}
             <div className={"signup-container"}>
@@ -160,7 +160,7 @@ function LoginPage() {
                 </form>
 
                 {/* Toggle between Login and Signup */}
-                <p onClick={() => setIsSignUp(!isSignUp)} style={{ cursor: "pointer", color: "blue" }}>
+                <p className={"link-to-account-creation"} onClick={() => setIsSignUp(!isSignUp)} style={{ cursor: "pointer", color: "blue" }}>
                     {isSignUp ? "Already have an account? Log in" : "Don't have an account? Sign up"}
                 </p>
 
